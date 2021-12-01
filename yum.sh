3746849f84853511698090086f0e8db960936247
Lam13o <lam13o@Lam13o.local>
2021-12-01 16:01:48 +0800

    delete someting

--- yum.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bac
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo  #aliyun_mirrors
-cat /etc/yum.repos.d/CentOS-Base.repo
